f38cd63b189daaadbe2ee23e2714784eaa0fde6b
fusco <emekaignatius5@gmail.com>
2024-04-04 15:37:24 +0100

    feat: added vehicles table

--- app/(protected)/vehicles/page.tsx
+++ app/(protected)/vehicles/page.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import TableDemo from '@/components/vehicles/ui-table'
+import React from 'react'
 
 export default function VehiclePage() {
-	return <div>VehiclePage</div>;
+  return (
+    <div><TableDemo/></div>
+  )
 }
