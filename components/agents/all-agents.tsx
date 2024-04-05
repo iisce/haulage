6df99440fbffc36209b7700b8785ada0f44e4ec4
emma <eoziegbe2@gmail.com>
2024-04-05 16:27:42 +0100

    updating agent

--- components/agents/all-agents.tsx
+++ components/agents/all-agents.tsx
@@ -2,15 +2,17 @@ import React from 'react'
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '../ui/table'
 import { Button } from '../ui/button'
 import Search from '../shared/search-component'
+import { AgentTable } from './agent-table'
 
 export const All_agents = () => {
   return (
     <>
-    <div>
-        <div className="flex gap-12 px-[20px] pt-[20px] w-full ">
+    <div className='px-[20px]'>
+        <div className="flex gap-12 pt-[20px] w-full ">
             <Search />
             <Button value={`Add Agent`}>Add Agent</Button>
         </div>
+        <AgentTable />
     </div>
     </>
   )
