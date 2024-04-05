e531fdc98cb296b7a4809a67616593c05f3139b6
oyeniran Ayobami Paul <ap.oyeniran@gmail.com>
2024-04-05 16:46:31 +0100

    fix: uncommented commented pages

--- app/(protected)/vehicles/[id]/page.tsx
+++ app/(protected)/vehicles/[id]/page.tsx
@@ -1,10 +1,5 @@
-// import ViewVehicleDetails from '@/components/vehicles/viewVehiclesDetails';
-// import React from 'react'
+import React from 'react';
 
-// export default async function VehicleId({ params }: {params: { id:string }; }) {
-//   return (
-//    <div className="w-full">
-//     <ViewVehicleDetails id={params.id}/>
-//    </div>
-//   )
-// }
+export default function SingleVehiclePage() {
+	return <div>SingleVehiclePage</div>;
+}
