89f285f7839bb7c504673402943aa963f1d55be9
fusco <emekaignatius5@gmail.com>
2024-04-11 21:23:23 +0100

    fix: fixed the error on admin page and ran build

--- app/(protected)/admins/new-admin/page.tsx
+++ app/(protected)/admins/new-admin/page.tsx
@@ -1,12 +1,12 @@
 import CreateAdminForm from "@/components/forms/new-admin-form";
 import React from "react";
 
-export default function NewAdmin() {
+export default function NewAdmin({ params }: {params:{id: string}}) {
   return (
     <div className="flex flex-col  pt-[20px] w-full">
       <div className="pl-5 py-3 text-lg">ADMIN INFORMATION</div>
       <div className=" my-4 mx-4">
-        <CreateAdminForm />
+        <CreateAdminForm id={params.id} />
       </div>
     </div>
   );
