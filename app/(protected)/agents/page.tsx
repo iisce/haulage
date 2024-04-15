750adfa2fb32bf6fa87420182b4834703fbdcbb1
oyeniran Ayobami Paul <ap.oyeniran@gmail.com>
2024-04-15 14:08:01 +0100

    fix: error on agents table

--- app/(protected)/agents/page.tsx
+++ app/(protected)/agents/page.tsx
@@ -1,19 +1,19 @@
 import { AgentTable } from '@/components/agents/agent-table';
 import React from 'react';
-import Link from "next/link";
+import Link from 'next/link';
 import Search from '@/components/shared/search-component';
 import { Button } from '@/components/ui/button';
 
-export default function AgentPage({params}:{params: {id:string};}) {
+export default function AgentPage({ params }: { params: { id: string } }) {
 	return (
-		<div className="px-[20px]">
-      <div className="flex gap-12 pt-[20px] w-full">
-        <Search />
-        <Link href="agents/new-agent">
-          <Button value={`Add Agent`}>Add Agent</Button>
-        </Link>
-      </div>
-      <AgentTable id={params.id} />
-    </div>
+		<div className='px-[20px]'>
+			<div className='flex gap-12 pt-[20px] w-full'>
+				<Search />
+				<Link href='agents/new-agent'>
+					<Button value={`Add Agent`}>Add Agent</Button>
+				</Link>
+			</div>
+			<AgentTable />
+		</div>
 	);
 }
