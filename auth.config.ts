c99d2ff5c491639e92a7e59ae85ddbac65d04afc
IISCE <iscedigitalconcept@gmail.com>
2024-11-07 11:09:47 +0100

    chore: merged dev with main

--- auth.config.ts
+++ auth.config.ts
@@ -1,41 +1,41 @@
-import type { NextAuthConfig } from 'next-auth';
-import Credentials from 'next-auth/providers/credentials';
-import { URLS, USER_ADMIN } from './constants';
-import { LoginSchema } from './schemas';
-import { axiosWithoutAuth } from './lib/axios.config';
+import axios from "axios";
+import type { NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { URLS } from "./constants";
+import { LoginSchema } from "./schemas";
 
 export default {
-	providers: [
-		Credentials({
-			async authorize(credentials) {
-				const validatedFields = LoginSchema.safeParse(credentials);
+     providers: [
+          Credentials({
+               async authorize(credentials) {
+                    const validatedFields = LoginSchema.safeParse(credentials);
 
-				if (validatedFields.success) {
-					const { email, password } = validatedFields.data;
-					// TODO: make API request to get user
+                    if (validatedFields.success) {
+                         const { email, password } = validatedFields.data;
+                         // TODO: make API request to get user
 
-					try {
-						const loginRequest = await axiosWithoutAuth.post(
-							URLS.auth.login,
-							{
-								email,
-								password,
-							}
-						);
-						if (!loginRequest.data.success) {
-							return null;
-						}
-						const user = loginRequest.data.data;
-						if (!user) return null;
-						return user;
-					} catch (error) {
-						console.log({ error });
-						return null;
-					}
-				}
+                         try {
+                              const loginRequest = await axios.post(
+                                   `https://haulage-api-latest.onrender.com${URLS.auth.login}`,
+                                   {
+                                        email,
+                                        password,
+                                   },
+                              );
+                              if (!loginRequest.data.success) {
+                                   return null;
+                              }
+                              const user = loginRequest.data.data;
+                              if (!user) return null;
+                              return user;
+                         } catch (error) {
+                              console.log({ error });
+                              return null;
+                         }
+                    }
 
-				return null;
-			},
-		}),
-	],
+                    return null;
+               },
+          }),
+     ],
 } satisfies NextAuthConfig;
